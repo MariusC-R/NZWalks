cec0d98dfd75f3824b2a9fb077af469984e69e83
MariusC-R <114995974+MariusC-R@users.noreply.github.com>
2022-11-18 12:12:27 +0000

    End of section 6

---
+++ NZWalks/NZWalks.API/Controllers/WalkDifficultyController.cs
@@ -0,0 +1,82 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.DTO;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WalkDifficultyController : Controller
+    {
+        private readonly IWalkDifficultyRepository walkDifficultyRepository;
+        private readonly IMapper mapper;
+
+        public WalkDifficultyController(IWalkDifficultyRepository walkDifficultyRepository, IMapper mapper)
+        {
+            this.walkDifficultyRepository = walkDifficultyRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var walkDifficulties = await walkDifficultyRepository.GetAsync();
+            mapper.Map<List<Models.DTO.WalkDifficulty>>(walkDifficulties);
+            return Ok(walkDifficulties);
+        }
+
+        [HttpGet("{id:guid}")]
+        [ActionName("GetSingleWalkDifficultyAsync")]
+        public async Task<IActionResult> GetSingleWalkDifficultyAsync(Guid id)
+        {
+            var walkDifficulty = await walkDifficultyRepository.GetSingleAsync(id);
+            if (walkDifficulty == null)
+            {
+                return NotFound();
+            }
+
+            var walkDifficultyDTO = mapper.Map<Models.DTO.WalkDifficulty>(walkDifficulty);
+            return Ok(walkDifficultyDTO);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddWalkDifficultyAsync([FromBody] AddWalkDifficultyRequest addWalkDifficultyRequest)
+        {
+            var walkDifficulty = new Models.Domain.WalkDifficulty() { Code = addWalkDifficultyRequest.Code };
+
+            walkDifficulty = await walkDifficultyRepository.AddAsync(walkDifficulty);
+            var walkDifficultyDTO = mapper.Map<Models.DTO.WalkDifficulty>(walkDifficulty);
+
+            return CreatedAtAction(nameof(GetSingleWalkDifficultyAsync), new { id = walkDifficultyDTO.Id }, walkDifficultyDTO);
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> UpdateWalkDifficultyAsync([FromRoute]Guid id, [FromBody] UpdateWalkDifficultyRequest updateWalkDifficultyRequest)
+        {
+            var walkDifficulty = new Models.Domain.WalkDifficulty() { Code = updateWalkDifficultyRequest.Code };
+
+            walkDifficulty = await walkDifficultyRepository.UpdateAsync(id, walkDifficulty);
+            if (walkDifficulty == null)
+            {
+                return NotFound();
+            }
+
+            var walkDifficultyDTO = mapper.Map<Models.DTO.WalkDifficulty>(walkDifficulty);
+            return Ok(walkDifficultyDTO);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> DeleteWalkDifficultyAsync(Guid id)
+        {
+            var walkDifficulty = await walkDifficultyRepository.DeleteAsync(id);
+            if (walkDifficulty == null)
+            {
+                return NotFound();
+            }
+
+            var walkDifficultyDTO = mapper.Map<Models.DTO.WalkDifficulty>(walkDifficulty);
+            return Ok(walkDifficultyDTO);
+        }
+    }
+}
---
+++ NZWalks/NZWalks.API/Repositories/WalkDifficultyRepository.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Data;
+
+namespace NZWalks.API.Repositories
+{
+    public class WalkDifficultyRepository : IWalkDifficultyRepository
+    {
+        private readonly NZWalksDbContext dbContext;
+
+        public WalkDifficultyRepository(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<WalkDifficulty> AddAsync(WalkDifficulty walkDifficulty)
+        {
+            walkDifficulty.Id = Guid.NewGuid();
+            await dbContext.WalkDifficulty.AddAsync(walkDifficulty);
+            await dbContext.SaveChangesAsync();
+            return walkDifficulty;            
+        }
+
+        public async Task<WalkDifficulty> DeleteAsync(Guid id)
+        {
+            var walkDifficulty = await dbContext.WalkDifficulty.FirstOrDefaultAsync(x => x.Id == id);
+            if (walkDifficulty == null)
+            {
+                return null;
+            }
+
+            dbContext.WalkDifficulty.Remove(walkDifficulty);
+            await dbContext.SaveChangesAsync();
+            return walkDifficulty;
+        }
+
+        public async Task<IEnumerable<WalkDifficulty>> GetAsync()
+        {
+            return await dbContext.WalkDifficulty.ToListAsync();
+        }
+
+        public async Task<WalkDifficulty> GetSingleAsync(Guid id)
+        {
+            var walkDifficulty = await dbContext.WalkDifficulty.FirstOrDefaultAsync(x => x.Id == id);
+            if (walkDifficulty == null)
+            {
+                return null;
+            }
+            return walkDifficulty;
+        }
+
+        public async Task<WalkDifficulty> UpdateAsync(Guid id, WalkDifficulty walkDifficulty)
+        {
+            var existingWalkDifficulty = await dbContext.WalkDifficulty.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (existingWalkDifficulty == null)
+            {
+                return null;
+            }
+
+            existingWalkDifficulty.Code = walkDifficulty.Code;
+            
+
+            await dbContext.SaveChangesAsync();
+            return existingWalkDifficulty;
+        }
+    }
+}
