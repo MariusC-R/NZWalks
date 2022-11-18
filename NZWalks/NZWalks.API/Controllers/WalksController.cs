781f304dc25dee7e2c562456d818017c8712b752
MariusC-R <114995974+MariusC-R@users.noreply.github.com>
2022-11-18 10:23:09 +0000

    End of section 5

---
+++ NZWalks/NZWalks.API/Controllers/WalksController.cs
@@ -0,0 +1,88 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.DTO;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WalksController : Controller
+    {
+        private readonly IWalksRepository walksRepository;
+        private readonly IMapper mapper;
+
+        public WalksController(IWalksRepository walksRepository, IMapper mapper)
+        {
+            this.walksRepository = walksRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllWalksAsync()
+        {
+            var walks = await walksRepository.GetAllAsync();
+
+            var walksDTO = mapper.Map<List<Models.DTO.Walk>>(walks);
+
+            return Ok(walksDTO);
+        }
+
+        [HttpGet("{id:guid}")]
+        [ActionName("GetSingleWalkAsync")]
+        public async Task<IActionResult> GetSingleWalkAsync(Guid id)
+        {
+            var walk = await walksRepository.GetAsync(id);
+
+            var walkDTO = mapper.Map<Models.DTO.Walk>(walk);
+
+            return Ok(walkDTO);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddWalkAsync([FromBody] AddWalkRequest addWalkRequest)
+        {
+            var walk = new Models.Domain.Walk()
+            {
+                Name = addWalkRequest.Name,
+                length = addWalkRequest.length,
+                RegionId = addWalkRequest.RegionId,
+                WalkDifficultyId = addWalkRequest.WalkDifficultyId,
+            };
+            walk = await walksRepository.AddAsync(walk);
+            var walkDTO = mapper.Map<Models.DTO.Walk>(walk);
+
+            return CreatedAtAction(nameof(GetSingleWalkAsync), new { id = walkDTO.Id }, walkDTO);
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> UpdateWalkAsync([FromRoute] Guid id, [FromBody] UpdateWalkRequest updateWalkRequest)
+        {
+            var walk = new Models.Domain.Walk()
+            {
+                Name = updateWalkRequest.Name,
+                length = updateWalkRequest.length,
+                RegionId = updateWalkRequest.RegionId,
+                WalkDifficultyId = updateWalkRequest.WalkDifficultyId
+            };
+
+            walk = await walksRepository.UpdateAsync(id, walk);
+            if (walk == null)
+            {
+                return NotFound();
+            }
+
+            var walkDTO = mapper.Map<Models.DTO.Walk>(walk);
+            return Ok(walkDTO);
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteWalkAsync(Guid id)
+        {
+            var walk = await walksRepository.DeleteAsync(id);
+
+            var walkDTO = mapper.Map<Models.DTO.Walk>(walk);
+            return Ok(walkDTO);
+        }
+    }
+}
---
+++ NZWalks/NZWalks.API/Repositories/WalksRepository.cs
@@ -0,0 +1,70 @@
+﻿using NZWalks.API.Models.Domain;
+using NZWalks.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace NZWalks.API.Repositories
+{
+    public class WalksRepository : IWalksRepository
+    {
+        private readonly NZWalksDbContext dbContext;
+
+        public WalksRepository(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<Walk> AddAsync(Walk walk)
+        {
+            walk.Id = Guid.NewGuid();
+            await dbContext.Walks.AddAsync(walk);
+            await dbContext.SaveChangesAsync();
+            return walk;
+        }
+
+        public async Task<Walk> DeleteAsync(Guid id)
+        {
+            var walk = await dbContext.Walks.FirstOrDefaultAsync(x => x.Id == id);
+            if (walk == null) 
+            {
+                return null;
+            }
+            dbContext.Remove(walk);
+            await dbContext.SaveChangesAsync();
+            return walk;
+        }
+
+        public async Task<IEnumerable<Walk>> GetAllAsync()
+        {
+            return await dbContext.Walks
+                .Include(x => x.Region)
+                .Include(x => x.WalkDifficulty)
+                .ToListAsync();
+        }
+
+        public async Task<Walk> GetAsync(Guid id)
+        {
+            return await dbContext.Walks
+                .Include(x => x.Region)
+                .Include(x => x.WalkDifficulty)
+                .FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<Walk> UpdateAsync(Guid id, Walk walk)
+        {
+            var existingWalk = await dbContext.Walks.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (existingWalk == null)
+            {
+                return null;
+            }
+
+            existingWalk.Name = walk.Name;
+            existingWalk.length = walk.length;
+            existingWalk.RegionId = walk.RegionId;
+            existingWalk.WalkDifficultyId = walk.WalkDifficultyId;
+
+            await dbContext.SaveChangesAsync();
+            return existingWalk;
+        }
+    }
+}
